9ccc4068525302b25063ae274de7e19738864009
FlyingAir <827199132@qq.com>
2017-11-27 10:23:01 +0800

    ignore

--- javaScript/tips.js
+++ javaScript/tips.js
@@ -1,16 +1,21 @@
 function isPrime(value) {
-  if(!isPrime.answers) isPrime.answers = {};
-  if(isPrime.answers[value]!=null){
-      return isPrime.answers[value]
-  }
-  var prime = value != 1;//1 不是素数
-  for(var i = 2;i<value;i++){
-      if(value%2===0){
-          prime = false;
-          break;
-      }
-  }
-  return isPrime.answers[value] = prime
+    if (!isPrime.answers) isPrime.answers = {};
+    if (isPrime.answers[value] != null) {
+        return isPrime.answers[value]
+    }
+    var prime = value != 1; //1 不是素数
+    for (var i = 2; i < value; i++) {
+        if (value % 2 === 0) {
+            prime = false;
+            break;
+        }
+    }
+    return isPrime.answers[value] = prime
 }
 console.log(isPrime(5));
 console.log(isPrime.answers[5]);
+
+function getFullName(user){
+    const {firstName,lastName} = user;
+    return `$(first)$(lastName)`;
+}
