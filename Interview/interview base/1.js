473b707b5e8a41ed9a4f530b3a0dbd1fc0e1133c
FlyingAir <827199132@qq.com>
2018-03-06 14:24:42 +0800

    vue原理

---
+++ Interview/interview base/1.js
@@ -0,0 +1,7 @@
+// 类型和类型转换
+typeof 0              // number
+typeof true           // boolean
+typeof 'Hello'        // string
+typeof Math           // object
+typeof null           // object  !!
+typeof Symbol('Hi')   // symbol (New ES6)
---
+++ javaScript/defineProperty.js
@@ -0,0 +1,7 @@
+var a = {};
+Object.defineProperty(a,'name',{
+    value:"joy",    
+    configurable:true,
+    writable : true,
+})
+console.log(a,a.name)
